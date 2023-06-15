0f09896acb6d1f4bde7208fe7c33a2a474db01e9
Suraj Kawale <srjsuraj1397@gmail.com>
2023-06-16 00:54:39 +0530

    list and map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 function App() {
   return (
     <>
-      <h1>props demo</h1>
-      <ListDemo/>
-       
+      <h1>Props Demo</h1>
+      <ListDemo />
     </>
   );
 }
 
 function ListDemo() {
-  let data ="Hello Universe";
-  let list=["delhi","culcutta", "mumabai"];
-  return(
+  let data = "Hello Universe";
+  let list = ["delhi", "calcutta", "chennai"];
+
+  return (
     <>
       <h1>{data}</h1>
 
-      {list.forEach((item)=>item)}
+      {/** Version.3 */}
+      {list.map((item) => (
+        <>
+          <h1>Hello World</h1>
+        </>
+      ))}
 
-      {/** map is input/output */}
-      {/** OUTPUT BCZ OF THIS */}
+      {/** Version.2 */}
+      {list.map((item) => "Hello" + item)}
 
-      {list.map((item)=>item)}
+      <hr />
+      {/** Version.1 */}
+      {list.map((item) => item)}
     </>
   );
 }
